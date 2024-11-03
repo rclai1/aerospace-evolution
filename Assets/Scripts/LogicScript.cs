19118d4bb9b86cd7e2e5989e2ff551ff4ecad18b
Christopher Wu <143110404+chriswx16@users.noreply.github.com>
2024-11-03 00:40:49 -0400

    Add files via upload

---
+++ Assets/Scripts/LogicScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public GameObject gameOverScreen;
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver() 
+    {
+        gameOverScreen.SetActive(true);
+    }
+}
